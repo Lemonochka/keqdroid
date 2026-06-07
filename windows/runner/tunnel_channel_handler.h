6a816be94acb3f4dc49397eb8b4aedee0fad9e54
Kequinq <lenorakirechaeva@gmail.com>
2026-06-07 22:49:15 +0400

    Export KeqdisRequestAutostartConnect()

--- windows/runner/tunnel_channel_handler.h
+++ windows/runner/tunnel_channel_handler.h
@@ -5,4 +5,7 @@
 
 void RegisterKeqdisTunnelChannel(flutter::FlutterEngine* engine);
 
+// Called when a second --autostart instance forwards connect to the running app.
+void KeqdisRequestAutostartConnect();
+
 #endif  // RUNNER_TUNNEL_CHANNEL_HANDLER_H_
